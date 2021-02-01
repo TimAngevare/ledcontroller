7b371b9e8f46657c2d7564b1081842a03611c69d
TimAngevare <t.angevare@students.uu.nl>
2021-02-01 11:29:40 +0100

    improved led

---
+++ controller.py
@@ -0,0 +1,41 @@
+import board
+import neopixel
+from time import sleep
+
+pixels = neopixel.NeoPixel(board.D18, 240)
+
+color = ()
+
+def setcolor(rgb):
+    global color
+    print(rgb)
+    color = rgb
+
+def fill(): 
+    pixels.fill(color)
+    print("FIll")
+
+def slide():
+    while True:
+        x = 239
+        while x > 0:
+            for i in range(x):
+                pixels[i] = color
+                try:
+                    pixels[i-1] = color
+                    pixels[i-2] = color
+                except:
+                    pass
+                sleep(0.001)
+                if i >= x-1:
+                    print("done")
+                    pixels[x] = color
+                    pixels[x-1] = color
+                    pixels[x-2] = color
+                    x -= 3
+                else:
+                    pixels[i] = (0,0,0)
+                    pixels[i-1] = (0,0,0)
+                    pixels[i-2] = (0,0,0)
+        pixels.fill((0,0,0))
+
